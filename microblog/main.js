32953ba069c6b3186cc09740a69a0711e497fa90
原宏之 <heartone@mba.local>
2024-04-29 16:46:42 +0900

    microblogステップ１と２を削除

--- microblog/main.js
+++
@@ -1,9 +0,0 @@
-$("form").on("submit", (e) => {
-  e.preventDefault();
-  if ($("textarea").val()) {
-    const li = document.createElement("li");
-    $("li").text($("textarea").val());
-    $("ul").prepend(li);
-    $("textarea").val("").focus();
-  }
-});
